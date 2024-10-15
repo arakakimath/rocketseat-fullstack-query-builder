e7c9743bf20f4f7c1959ad7f91489ca26a8e7c04
Matheus Arakaki <arakakimath@gmail.com>
2024-10-15 17:16:08 -0400

    updating data with PUT method

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,32 @@
-import express, { Request, Response } from "express"
-import { knex } from "./database/knex"
+import express, { Request, Response } from "express";
+import { knex } from "./database/knex";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.post("/courses", async (request: Request, response: Response) => {
-  const { name } = request.body
+  const { name } = request.body;
 
-  await knex("courses").insert({ name })
+  await knex("courses").insert({ name });
   // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name])
 
-  response.status(201).json()
-})
+  response.status(201).json();
+});
 
 app.get("/courses", async (request: Request, response: Response) => {
   //const courses = await knex.raw("SELECT * FROM courses")
-  const courses = await knex("courses").select().orderBy("name", "desc")
+  const courses = await knex("courses").select().orderBy("name", "desc");
 
-  response.json(courses)
-})
+  response.json(courses);
+});
 
-app.listen(3333, () => console.log(`Server is running on port 3333`))
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+
+  await knex("courses").update({ name }).where({ id });
+
+  return response.json();
+});
+
+app.listen(3333, () => console.log(`Server is running on port 3333`));
